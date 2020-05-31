ebf6118fe79be857138f575d16eb8d70d0e55b0a
witcher <sergeev.davutt@gmail.com>
2020-06-01 02:03:29 +0300

    Интергация верстки пример с Bootstrap 4 и настройка проекта

---
+++ resources/views/layouts.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Пример на bootstrap 4: Простой одностраничный шаблон для фотогалереи, портфолио и многого другого.">
+
+    <title>{{ config('app.name', 'My Books') }}</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="css/posts.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+  @include('layouts.header')
+
+  @yield('content')
+
+  @include('layouts.footer')
+
+</body>
+</html>
