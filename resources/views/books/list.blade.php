59f1da580f4fbc07a192a9c16c25b6c06d4f6d59
witcher <sergeev.davutt@gmail.com>
2020-06-01 16:58:54 +0300

    Исправлены ссылки и вывод кнопок регистрация и вход

--- resources/views/books/list.blade.php
+++ resources/views/books/list.blade.php
@@ -8,8 +8,10 @@
             <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
             <p>
                 @guest
-                    <a href="/login" class="btn btn-primary my-2">Log in</a>
-                    <a href="/register" class="btn btn-secondary my-2">Register</a>
+                    <a href="{{ route('login') }}" class="btn btn-primary my-2">{{ __('Login') }}</a>
+                    @if (Route::has('register'))
+                        <a class="btn btn-secondary my-2" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    @endif                    
                 @else
                     <p class="lead text-muted">Hi, {{Auth::user()->name}}!</p>
                     <a class="btn btn-primary my-2" href="{{ route('logout') }}"
