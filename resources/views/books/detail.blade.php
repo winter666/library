d08d9e7ea3793d30d3645e8a8af0be81199f4859
witcher <sergeev.davutt@gmail.com>
2020-06-02 00:10:46 +0300

    Детальная странница просмотра книг: редактировал верстку + добавил стили

--- resources/views/books/detail.blade.php
+++ resources/views/books/detail.blade.php
@@ -3,8 +3,19 @@
 @section('content')
 
 <div class="container">
-	<h3>{{$book->name}}</h3>
-	<p>{{$book->description}}</p>
+    <div class="detail_content">
+        <h2 class="title">{{$book->name}}</h2>
+        <div class="detal_author">
+        	<span>Author:</span>
+        	<a href="#" class="author_detail">
+        		<span class="author_value"></span>
+        	</a>
+        </div>
+        <div>
+            <h4>Description:</h4>
+            <p class="detail_desc">{{$book->description}}</p>
+        </div>
+    </div>    
 </div>
 
 @endsection
