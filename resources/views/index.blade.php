10be86e97125532dcc3c278c26fc1802bab505b0
witcher <sergeev.davutt@gmail.com>
2020-06-03 02:41:58 +0300

    Удален дублирующийся код, подключен через include

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,16 +2,6 @@
 
 @section('content')
 
-<section class="jumbotron text-center">
-    <div class="container">
-        <h1 class="jumbotron-heading">{{ config('app.name', 'My Books') }}</h1>
-        <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-        <p>
-            @auth
-                <p class="lead text-muted">Hi, {{Auth::user()->name}}!</p>                    
-            @endauth    
-        </p>
-    </div>
-</section>
+@include('layouts.section')
 
 @endsection
