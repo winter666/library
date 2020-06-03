43e2fde6af63ee2338d6aeab20fb80f7ab16ecb8
witcher <sergeev.davutt@gmail.com>
2020-06-03 20:49:06 +0300

    Форма добавления авторов

--- resources/views/authors/list.blade.php
+++ resources/views/authors/list.blade.php
@@ -5,6 +5,27 @@
 @include('layouts.section')
 
 <div class="container">
+
+    <div class="add_book-btn">
+        <button class="btn btn-sm btn-outline-secondary" id="evt-modal">Add Author</button>
+    </div>    
+    <div class="background_shadow">
+        <div class="modal-window">
+            <div class="close">
+                <span class="close_btn">X</span>
+            </div>
+            <div class="form-create_book">
+                <form action="#" method="POST">
+                    <h4 class="form_title">Create Author</h4>
+                    <div class="input-create">
+                        <label for="author_name">Name</label><br>
+                        <input type="text" name="name" id="author_name">
+                    </div>
+                    <button class="btn btn-sm btn-outline-secondary" id="evt-create">Add</button>
+                </form>
+            </div>    
+        </div>     
+    </div>    
     <table class="table table-dark">
         <thead>
             <tr>
